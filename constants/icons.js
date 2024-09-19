be8dddfcbbe72032b9661eb0c050a5238975dad4
LilChilChill <cuongchi404@gmail.com>
2024-09-19 20:38:12 +0700

    little update

--- constants/icons.js
+++ constants/icons.js
@@ -1,2 +1,3 @@
 export const uriMenuList = require('../assets/icons/menu.png')
 export const uriEdit = require('../assets/icons/pencil.png')
+export const uriSearch = require('../assets/icons/search.png')
--- modules/Home/index.js
+++ modules/Home/index.js
@@ -8,7 +8,7 @@ const HomeScreen = ({ navigation }) => {
     <View style={{backgroundColor: '#FFF', flex: 1}}>
       <View style={styles().header}>
         <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-          <TouchableOpacity style={styles().icons_bg}>
+          <TouchableOpacity style={styles().icons_bg} onPress={() => navigation.navigate('Detail')}>
             <Image source={constants.icons.uriMenuList} style={styles().icons} />
           </TouchableOpacity>
           <Text style={styles().header_text}>
@@ -21,10 +21,19 @@ const HomeScreen = ({ navigation }) => {
         </TouchableOpacity>
       </View>
 
-      <View style={{display: 'flex', alignItems: 'center', marginVertical: 20}}>
-        <TextInput style={styles().search} placeholder='Tìm Kiếm'>
-
+      {/* <View style={styles().search}>
+        <Image source={constants.icons.uriSearch} style={styles().icons} />
+        <TextInput style={styles().search_input} placeholder='Tìm Kiếm'>
         </TextInput>
+      </View> */}
+
+      <View style={styles().search_bg}>
+        <View style={styles().search}>
+          <Image source={constants.icons.uriSearch} style={styles().icons} />
+          <TextInput style={styles().search_input} placeholder='Tìm kiếm' placeholderTextColor={'#8f8f8f'}>
+            
+          </TextInput>
+        </View>
       </View>
     </View>
   );
@@ -34,16 +43,16 @@ export default HomeScreen;
 const styles = () => StyleSheet.create({
     icons_bg:{
       backgroundColor: '#e7e6ef',
-      width: 50,
-      height: 50,
+      width: 40,
+      height: 40,
       borderRadius: 25,
       padding: 5,
       alignItems: 'center',
       justifyContent: 'center'
     },  
     icons:{
-      width: 25,
-      height: 25,
+      width: 20,
+      height: 20,
       resizeMode: 'contain'
     },
     header:{
@@ -53,21 +62,29 @@ const styles = () => StyleSheet.create({
       paddingTop: 20,
     },
     header_text:{
-      fontSize: 25,
+      fontSize: 18,
       fontWeight: 'bold',
       color: '#000',
       marginLeft: 20
     },
-    search:{
+    search_bg:{
+      marginVertical: 20,
+      alignSelf: 'center',
       backgroundColor: '#e7e6ef',
       height: 50,
       width: 300,
+      borderRadius: 25,
+    },
+    search:{
+      marginLeft: 20,
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      fontSize: 16,
+    },
+    search_input:{
       fontSize: 16,
       borderRadius: 20,
-      paddingLeft: 20,
       color: '#000',
-      justifyContent: 'center',
-      alignContent: 'center',
-      alignItems: 'center',
     }
 })
